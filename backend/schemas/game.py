f14c383df97063dbcd722677f0a5b5c0309303aa
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-06-29 12:13:39 +0530

    Updated the SChemas

--- backend/schemas/game.py
+++ backend/schemas/game.py
@@ -1,12 +1,39 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional, Literal
 
-# For POST /prompt
 class PromptRequest(BaseModel):
-    type: str  # "challenge", "enemy", or "mystery"
+    type: str  # challenge, enemy, mystery
     level: int
 
-# For GET /start
 class StartResponse(BaseModel):
     message: str
 
+# Response schemas
+class GameEvent(BaseModel):
+    title: str
+    description: str
+
+class PlayRequest(BaseModel):
+    user_id: str
+    planet_id: str
+    level: int
+
+class PlayResponse(BaseModel):
+    planet_id: str
+    planet_name: str
+    difficulty: str
+    challenges: List[GameEvent]
+    enemies: List[GameEvent]
+    mysteries: List[GameEvent]
+
+
+
+class Puzzle(BaseModel):
+    puzzle_type: Literal["riddle", "pattern", "logic"]
+    title: str
+    question: str
+    choices: List[str]
+    answer: str
+
+class PuzzleResponse(BaseModel):
+    puzzle: Puzzle
