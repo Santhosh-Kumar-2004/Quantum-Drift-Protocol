00d14eaeb804006345b7b6b1c2a91763c15f393a
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-06-29 10:36:10 +0530

    Add files via upload

---
+++ backend/data/planets.py
@@ -0,0 +1,32 @@
+planets = [
+    {
+        "planet_id": "planet_1",
+        "planet_name": "Zerion Prime",
+        "level_number": 1,
+        "difficulty": "Easy"
+    },
+    {
+        "planet_id": "planet_2",
+        "planet_name": "Nebulon-6",
+        "level_number": 2,
+        "difficulty": "Medium"
+    },
+    {
+        "planet_id": "planet_3",
+        "planet_name": "Xal'tar Void",
+        "level_number": 3,
+        "difficulty": "Hard"
+    },
+    {
+        "planet_id": "planet_4",
+        "planet_name": "Chrona Rift",
+        "level_number": 4,
+        "difficulty": "Very Hard"
+    },
+    {
+        "planet_id": "planet_5",
+        "planet_name": "Oblivion Gate",
+        "level_number": 5,
+        "difficulty": "Nightmare"
+    }
+]
